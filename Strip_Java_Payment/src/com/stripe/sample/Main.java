439b1664aeaa2ffc67e66e69b76653cef50920c7
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-12-13 13:38:32 +0200

    Update Main.java

--- Strip_Java_Payment/src/com/stripe/sample/Main.java
+++ Strip_Java_Payment/src/com/stripe/sample/Main.java
@@ -1,11 +1,5 @@
 package com.stripe.sample;
 
-import java.nio.file.Paths;
-
-import static spark.Spark.post;
-import static spark.Spark.port;
-import static spark.Spark.staticFiles;
-
 import com.stripe.Stripe;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
