37343390bde020fdaaa6e7a60f862ff944ecf787
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-12-24 16:24:53 +0200

    Update RazorpayExample.java

--- Razorpay/RazorpayExample.java
+++ Razorpay/RazorpayExample.java
@@ -1,26 +1,76 @@
 import com.razorpay.RazorpayClient;
 import com.razorpay.Order;
+import com.razorpay.Customer;
 import com.razorpay.RazorpayException;
+import com.razorpay.Payment;
 import org.json.JSONObject;
+
+import java.util.List;
 
 public class RazorpayExample {
 
     public static void main(String[] args) {
-        // Step 1: Initialize Razorpay
         try {
-            RazorpayClient razorpay = new RazorpayClient("your_api_key", "your_api_secret");
+            // Initialize Razorpay
+            RazorpayClient razorpay = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
 
-            // Step 2: Create a Payment
-            JSONObject options = new JSONObject();
-            options.put("amount", 50000);  // amount in paise (50000 paise = Rs 500)
-            options.put("currency", "INR");
-            options.put("receipt", "order_rcptid_11");
-            options.put("payment_capture", 1); // 1 for automatic capture, 0 for manual capture
+            // Create a payment order
+            JSONObject orderRequest = new JSONObject();
+            orderRequest.put("amount", 50000); // amount in the smallest currency unit
+            orderRequest.put("currency", "INR");
+            orderRequest.put("receipt", "order_rcptid_11");
 
-            Order order = razorpay.Orders.create(options);
+            Order order = razorpay.Orders.create(orderRequest);
             System.out.println("Order ID: " + order.get("id"));
+
+            // Handle payment order exception
         } catch (RazorpayException e) {
-            e.printStackTrace();
+            System.out.println("Payment Order Exception: " + e.getMessage());
+        }
+
+        // Handle 'payment.submit' event
+        var rzp = new Razorpay(options);
+        rzp.on('payment.submit', function (data) {
+            if (data.method === 'bank_transfer') {
+                // User has selected Bank Transfer
+            }
+        });
+
+        // Fetch payments with 'card' expansion
+        try {
+            RazorpayClient instance = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+            JSONObject cardParams = new JSONObject();
+            cardParams.put("expand[]", "card");
+            List<Payment> cardPayments = instance.payments.fetchAll(cardParams);
+        } catch (RazorpayException e) {
+            System.out.println("Fetch Card Payments Exception: " + e.getMessage());
+        }
+
+        // Fetch payments with 'emi' expansion
+        try {
+            RazorpayClient instance = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+            JSONObject emiParams = new JSONObject();
+            emiParams.put("expand[]", "emi");
+            List<Payment> emiPayments = instance.payments.fetchAll(emiParams);
+        } catch (RazorpayException e) {
+            System.out.println("Fetch EMI Payments Exception: " + e.getMessage());
+        }
+
+        // Fetch a specific payment downtime by ID
+        try {
+            RazorpayClient instance = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+            String DowntimeId = "down_F7LroRQAAFuswd";
+            Payment specificPayment = instance.payments.fetchPaymentDowntimeById(DowntimeId);
+        } catch (RazorpayException e) {
+            System.out.println("Fetch Specific Payment Downtime Exception: " + e.getMessage());
+        }
+
+        // Fetch all payment downtimes
+        try {
+            RazorpayClient instance = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+            List<Payment> allDowntimes = instance.payments.fetchPaymentDowntime();
+        } catch (RazorpayException e) {
+            System.out.println("Fetch All Payment Downtimes Exception: " + e.getMessage());
         }
     }
 }
