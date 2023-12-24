0fbb2e0102022a11e4da147e0b438806be8a3075
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-12-24 12:48:20 +0200

    Update simple_example.java
    fixing bugs

--- Wepay/simple_example.java
+++ Wepay/simple_example.java
@@ -1,46 +1,51 @@
-import com.wepay.WePay;                     // WePay object needed for API initialization
-import com.wepay.model.*;                   // contains call classes and all API call functions
-import com.wepay.model.data.*;              // contains all data objects needed for making calls
+import com.wepay.WePay;
+import com.wepay.model.Account;
+import com.wepay.model.Checkout;
 import com.wepay.model.data.AccountData;
-import com.wepay.model.data.CheckoutData
-import com.wepay.net.WePayResource;         // network resource used to execute calls
-import com.wepay.exception.WePayException;  // handles WePay exceptions
-import org.json.*;                          // SDK uses JSON when handling API call parameters
-import com.google.gson.*;                   // SDK uses GSON for building objects from API responses
+import com.wepay.model.data.CheckoutData;
+import com.wepay.model.data.OAuth2Data;
+import com.wepay.net.OAuth2;
+import com.wepay.exception.WePayException;
 
 public class WePayExample {
 
-  WePay wepay = new WePay();
-  wepay.initialize(myClientId, myClientSecret, useStageEnv);
-  
-  // create a new account
-  AccountData aData = new AccountData();
-  aData.name = "Test Person";                          // required parameter for this API call
-  aData.description = "This is an example account";    // required parameter for this API call
-  aData.type = "personal";                             // optional parameter for this API call
-  Account newAccount = Account.create(aData, myAccessToken);
-  
-  // get an accessToken for the new account
-  OAuth2Data data = new OAuth2Data();
-  data.redirectUri = "http://www.mywebsite.com/oauth2/";
-  data.scope = "manage_accounts,collect_payments,view_user,preapprove_payments,send_money";
-  String sendUserToThisURL = OAuth2.authorize(data, null);
-  //send user to the returned URL to complete OAuth2 authorization form
-  
-  //on completion of authorization, user is redirected to redirectUri with a code parameter
-  //once you have the code you can exchange it for an access token
-  OAuth2Data data = new OAuth2Data();
-  data.redirectUri = "http://www.mywebsite.com/oauth2/";
-  data.code = code;
-  String userNewAccessToken = OAuth2.token(data, null);
-  
-  // create a new checkout
-  CheckoutData cData = new CheckoutData();
-  cData.accountId = newAccount.getAccountId();       // use a returned object to access information
-  cData.shortDescription = "Soccer Ball Purchase";
-  cData.type = "GOODS";
-  cData.amount = 29.95;
-  cData.currency = "USD";
-  Checkout newCheckout = Checkout.create(cData, userNewAccessToken);
-  
+    public static void main(String[] args) {
+        WePay wepay = new WePay();
+        wepay.initialize(myClientId, myClientSecret, useStageEnv);
+
+        // create a new account
+        AccountData aData = new AccountData();
+        aData.name = "Test Person";                          // required parameter for this API call
+        aData.description = "This is an example account";    // required parameter for this API call
+        aData.type = "personal";                             // optional parameter for this API call
+        try {
+            Account newAccount = Account.create(aData, myAccessToken);
+
+            // get an accessToken for the new account
+            OAuth2Data oauth2Data = new OAuth2Data();
+            oauth2Data.redirectUri = "http://www.mywebsite.com/oauth2/";
+            oauth2Data.scope = "manage_accounts,collect_payments,view_user,preapprove_payments,send_money";
+            String sendUserToThisURL = OAuth2.authorize(oauth2Data, null);
+            // send the user to the returned URL to complete OAuth2 authorization form
+
+            // on completion of authorization, the user is redirected to redirectUri with a code parameter
+            // once you have the code, you can exchange it for an access token
+            OAuth2Data tokenData = new OAuth2Data();
+            tokenData.redirectUri = "http://www.mywebsite.com/oauth2/";
+            tokenData.code = code;
+            String userNewAccessToken = OAuth2.token(tokenData, null);
+
+            // create a new checkout
+            CheckoutData cData = new CheckoutData();
+            cData.accountId = newAccount.getAccountId();       // use a returned object to access information
+            cData.shortDescription = "Soccer Ball Purchase";
+            cData.type = "GOODS";
+            cData.amount = 29.95;
+            cData.currency = "USD";
+            Checkout newCheckout = Checkout.create(cData, userNewAccessToken);
+
+        } catch (WePayException e) {
+            e.printStackTrace();
+        }
+    }
 }
