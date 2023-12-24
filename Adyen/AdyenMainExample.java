8b08bc748f6331d33f1e1950d23522971ddbcdaa
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2023-12-24 16:40:00 +0200

    Update AdyenMainExample.java

--- Adyen/AdyenMainExample.java
+++ Adyen/AdyenMainExample.java
@@ -1,7 +1,7 @@
-// Import the required classes
 import com.adyen.Client;
 import com.adyen.enums.Environment;
 import com.adyen.service.checkout.PaymentsApi;
+import com.adyen.model.checkout.*;
 import com.adyen.model.checkout.Amount;
 import com.adyen.model.checkout.CardDetails;
 import com.adyen.model.checkout.CheckoutCard;
@@ -9,7 +9,7 @@ import com.adyen.model.checkout.CheckoutPaymentsRequest;
 import com.adyen.model.checkout.CheckoutPaymentsResponse;
 import com.adyen.model.checkout.RequestOptions;
 
-public class AdyenMainExample {
+public class AdyenPaymentExample {
     // Setup the client and service.
     Client client = new Client("YOUR_API_KEY", Environment.TEST);
     PaymentsApi paymentsApi = new PaymentsApi(client);
@@ -34,4 +34,19 @@ public class AdyenMainExample {
 
     // Make a request to the /payments endpoint.
     CheckoutPaymentsResponse checkoutPaymentsResponse = paymentsApi.payments(paymentRequest, requestOptions);
+
+    // Additional payment-related methods (Add more as needed)
+    // For example:
+    // 1. Capture a payment
+    CaptureRequest captureRequest = new CaptureRequest();
+    captureRequest.setAmount(amount);
+    CaptureResponse captureResponse = paymentsApi.capture(checkoutPaymentsResponse.getPspReference(), captureRequest, requestOptions);
+
+    // 2. Refund a payment
+    RefundRequest refundRequest = new RefundRequest();
+    refundRequest.setAmount(amount);
+    RefundResponse refundResponse = paymentsApi.refund(checkoutPaymentsResponse.getPspReference(), refundRequest, requestOptions);
+
+    // 3. Cancel a payment
+    CancelOrRefundResponse cancelResponse = paymentsApi.cancelOrRefund(checkoutPaymentsResponse.getPspReference(), requestOptions);
 }
